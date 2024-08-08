6dec36a76159930d696057ce6a5304adaf74563e
jacky <jackywu526@gmail.com>
2024-08-08 19:19:01 +0800

    feat(1A2B.vue): 新增 示範遊戲 1A2B

---
+++ src/views/1A2B.vue
@@ -0,0 +1,43 @@
+<script setup lang='ts'>
+import ContainWrap from '../components/ContainWrap.vue';
+</script>
+
+<template>
+  <div>
+    <ContainWrap
+      :title="'1A2B'"
+    >
+      <template #info>
+        <div class="flex justify-center items-center">
+          game info
+        </div>
+      </template>
+      <template #code>
+        <pre v-highlightjs>
+        <code class="javascript">
+function guessNumber(guess, answer) {
+  if (!/^\d{4}$/.test(guess)) {
+    return '請輸入四位數';
+  }
+
+  const answerSet = new Set([...answer]);
+  const guessSet = new Set([...guess]);
+
+  const [A, B] = answer.split('').reduce(([a, b], num, index) => {
+    if (num === guess[index]) a++;
+    if (answer.includes(num)) b++;
+    return [a, b];
+  }, [0, 0]);
+
+  return `${A}A${B}B`;
+}
+</code>
+</pre>
+      </template>
+    </ContainWrap>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+
+</style>
