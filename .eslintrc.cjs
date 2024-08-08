b29a409c2a8d15e77e3556ac6e8709118e7bb4ba
jacky <jackywu526@gmail.com>
2024-08-08 12:04:50 +0800

    feat(eslint): eslint import airbnb

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -3,7 +3,10 @@ require('@rushstack/eslint-patch/modern-module-resolution')
 
 module.exports = {
   root: true,
-  'extends': [
+  env: {
+    node: true,
+  },
+  extends: [
     'plugin:vue/vue3-essential',
     'eslint:recommended',
     '@vue/eslint-config-typescript',
@@ -21,6 +24,9 @@ module.exports = {
     }
   ],
   parserOptions: {
-    ecmaVersion: 'latest'
-  }
-}
+    ecmaVersion: 'latest',
+  },
+  rules: {
+    'vue/multi-word-component-names': 'off',
+  },
+};
