3f9eacae2cfa21dca8d92aa3ebf6995975c434b3
jacky <jackywu526@gmail.com>
2024-08-08 19:15:55 +0800

    feat(vue3-highlightjs): install vue3-highlightjs

--- env.d.ts
+++ env.d.ts
@@ -1,1 +1,6 @@
 /// <reference types="vite/client" />
+
+declare module 'vue3-highlightjs' {
+  const VueHighlightJS: any;
+  export default VueHighlightJS;
+}
--- src/main.ts
+++ src/main.ts
@@ -8,10 +8,14 @@ import 'element-plus/dist/index.css';
 import App from './App.vue';
 import router from './router';
 
+import VueHighlightJS from 'vue3-highlightjs';
+import 'highlight.js/styles/solarized-light.css';
+
 const app = createApp(App);
 
 app.use(createPinia());
 app.use(router);
 app.use(ElementPlus);
+app.use(VueHighlightJS);
 
 app.mount('#app');
