3b250e01978d67d2d3cbe3410019f9b05ba4e4e9
jacky <jackywu526@gmail.com>
2024-08-09 17:50:24 +0800

    feat(App.vue): 新增 version from package.json

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,13 @@
 <script setup lang="ts">
 import Layout from './layout/Layout.vue';
+import { version } from '../package.json';
 </script>
 
 <template>
   <Layout />
+  <div class="absolute right-[5px] bottom-[5px] text-xs text-gray-500">
+    ver {{ version }}
+  </div>
 </template>
 
 <style scoped>
