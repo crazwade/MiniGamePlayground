fb5753717e10f88d27fd2616c4ca1a19e85df9bf
jacky <jackywu526@gmail.com>
2024-08-16 01:02:05 +0800

    feat(@element-plus/icons-vue): install and import @element-plus/icons-vue

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,7 @@ import { createApp } from 'vue';
 import { createPinia } from 'pinia';
 import ElementPlus from 'element-plus';
 import 'element-plus/dist/index.css';
+import * as ElementPlusIconsVue from '@element-plus/icons-vue'
 
 import App from './App.vue';
 import router from './router';
@@ -12,6 +13,10 @@ import VueHighlightJS from 'vue3-highlightjs';
 import 'highlight.js/styles/solarized-light.css';
 
 const app = createApp(App);
+
+for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
+  app.component(key, component)
+}
 
 app.use(createPinia());
 app.use(router);
